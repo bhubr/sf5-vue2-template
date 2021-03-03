cc4b01d6049a5379fccfc5ff82a3fae6d7978bde
Benoît Hubert <benoithubert@gmail.com>
2021-03-04 00:52:14 +0100

    Added Dotenv Webpack to handle env vars

--- assets/services/apiServices.js
+++ assets/services/apiServices.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api',
+    baseURL: `${process.env.API_BASE_URL}/api`,
     withCredentials: false,
     headers: {
         Accept: 'application/json',
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const Encore = require('@symfony/webpack-encore');
+const Dotenv = require('dotenv-webpack');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -55,6 +56,8 @@ Encore
         config.useBuiltIns = 'usage';
         config.corejs = 3;
     })
+
+    .addPlugin(new Dotenv( { path: './.env.vue.local', systemvars: true, defaults: true } ))
 
     // enables Sass/SCSS support
     //.enableSassLoader()
